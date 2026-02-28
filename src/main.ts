1b9db5bb4dd2be8aad5cc3ff5d39bc020c851afb
Zapharaos <matthieu.freitag@gmail.com>
2026-02-28 21:17:14 +0100

    feat(main): disable console logs in production environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from '@environments/environment';
+
+if (environment.production) {
+  const noop = () => {};
+  console.log   = noop;
+  console.info  = noop;
+  console.debug = noop;
+  console.warn  = noop;
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
